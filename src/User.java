cc33ef5524269ff54015c2d9c442f1812a993c74
BuildTools <unconfigured@null.spigotmc.org>
2023-06-21 14:28:13 +0300

    added class User

---
+++ src/User.java
@@ -0,0 +1,27 @@
+public class User {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String sex;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phoneNumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
